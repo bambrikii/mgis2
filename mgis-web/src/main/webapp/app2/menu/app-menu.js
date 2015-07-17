fdbcd7070cfae3139316bccbafbc7745be6b8db2
Alexander Arakelyan <bambrikii@gmail.com>
2015-07-17 14:43:05 +0300

    - active menu items.

--- mgis-web/src/main/webapp/app2/menu/app-menu.js
+++ mgis-web/src/main/webapp/app2/menu/app-menu.js
@@ -1,9 +1,29 @@
 angular.module("mgis.menu.main", [ "ui.router", "ui.bootstrap" ]) //
 .controller("MainMenuCtrl", function($scope, $location) {
 	$scope.navCollapsed = false;
-	$scope.isogdMenuActive = function() {
-		return $location.path().indexOf("/isogd") > -1;
+	function resetMenus() {
+		$scope.isogdMenuActive = false;
+		$scope.landsMenuActive = false;
+		$scope.oksMenuActive = false;
+		$scope.administrationMenuActive = false;
 	}
+	$scope.isogdMenuClicked = function() {
+		resetMenus();
+		$scope.isogdMenuActive = true;
+	}
+	$scope.landsMenuClicked = function() {
+		resetMenus();
+		$scope.landsMenuActive = true;
+	}
+	$scope.oksMenuClicked = function() {
+		resetMenus();
+		$scope.oksMenuActive = true;
+	}
+	$scope.administrationMenuClicked = function() {
+		resetMenus();
+		$scope.administrationMenuActive = true;
+	}
+
 	$scope.toggleNavCollapsed = function() {
 		$scope.navCollapsed = !$scope.navCollapsed;
 	}
