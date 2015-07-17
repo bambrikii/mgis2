f09e1d9201566e5e615be01eb1f6d0fbf1de81cc
Alexander Arakelyan <bambrikii@gmail.com>
2015-07-17 12:15:32 +0300

    - doc icon, ...

--- mgis-business/src/main/java/ru/sovzond/mgis2/preview/ImageManipulationBean.java
+++ mgis-business/src/main/java/ru/sovzond/mgis2/preview/ImageManipulationBean.java
@@ -1,15 +1,18 @@
 package ru.sovzond.mgis2.preview;
 
-import org.springframework.stereotype.Service;
-
-import javax.imageio.ImageIO;
-import java.awt.*;
+import java.awt.Graphics2D;
+import java.awt.Image;
 import java.awt.image.BufferedImage;
 import java.awt.image.ImageObserver;
 import java.awt.image.VolatileImage;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+
+import javax.imageio.ImageIO;
+
+import org.springframework.stereotype.Service;
 
 /**
  * Created by Alexander Arakelyan on 16.07.15.
@@ -17,56 +20,75 @@ import java.io.IOException;
 @Service
 public class ImageManipulationBean {
 
+	public static final int MAX_DIMENSION = 100;
 
-    public static final int MAX_DIMENSION = 100;
+	public byte[] createThumbnailFromImage(byte[] bytes) throws IOException {
+		BufferedImage read = ImageIO.read(new ByteArrayInputStream(bytes));
+		Image image = read.getScaledInstance(MAX_DIMENSION, MAX_DIMENSION * read.getHeight() / read.getWidth(), BufferedImage.SCALE_SMOOTH);
+		ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+		ImageIO.write(toBufferedImage(image, BufferedImage.TYPE_INT_ARGB), "png", byteArrayOutputStream);
+		return byteArrayOutputStream.toByteArray();
+	}
 
-    public byte[] createThumbnail(byte[] bytes) throws IOException {
-        BufferedImage read = ImageIO.read(new ByteArrayInputStream(bytes));
-        Image image = read.getScaledInstance(MAX_DIMENSION, MAX_DIMENSION * read.getHeight() / read.getWidth(), BufferedImage.SCALE_SMOOTH);
-        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-        ImageIO.write(toBufferedImage(image, BufferedImage.TYPE_INT_ARGB), "png", byteArrayOutputStream);
-        return byteArrayOutputStream.toByteArray();
-    }
+	public BufferedImage toBufferedImage(final Image image, final int type) {
+		if (image instanceof BufferedImage) { return (BufferedImage) image; }
+		if (image instanceof VolatileImage) { return ((VolatileImage) image).getSnapshot(); }
+		loadImage(image);
+		final BufferedImage buffImg = new BufferedImage(image.getWidth(null), image.getHeight(null), type);
+		final Graphics2D g2 = buffImg.createGraphics();
+		g2.drawImage(image, null, null);
+		g2.dispose();
+		return buffImg;
+	}
 
-    public BufferedImage toBufferedImage(final Image image, final int type) {
-        if (image instanceof BufferedImage)
-            return (BufferedImage) image;
-        if (image instanceof VolatileImage)
-            return ((VolatileImage) image).getSnapshot();
-        loadImage(image);
-        final BufferedImage buffImg = new BufferedImage(image.getWidth(null), image.getHeight(null), type);
-        final Graphics2D g2 = buffImg.createGraphics();
-        g2.drawImage(image, null, null);
-        g2.dispose();
-        return buffImg;
-    }
+	private void loadImage(final Image image) {
+		class StatusObserver implements ImageObserver {
+			boolean imageLoaded = false;
 
-    private void loadImage(final Image image) {
-        class StatusObserver implements ImageObserver {
-            boolean imageLoaded = false;
+			@Override
+			public boolean imageUpdate(final Image img, final int infoflags, final int x, final int y, final int width, final int height) {
+				if (infoflags == ALLBITS) {
+					synchronized (this) {
+						imageLoaded = true;
+						notify();
+					}
+					return true;
+				}
+				return false;
+			}
+		}
+		final StatusObserver imageStatus = new StatusObserver();
+		synchronized (imageStatus) {
+			if (image.getWidth(imageStatus) == -1 || image.getHeight(imageStatus) == -1) {
+				while (!imageStatus.imageLoaded) {
+					try {
+						imageStatus.wait();
+					} catch (InterruptedException ex) {
+					}
+				}
+			}
+		}
+	}
 
-            public boolean imageUpdate(final Image img, final int infoflags,
-                                       final int x, final int y, final int width, final int height) {
-                if (infoflags == ALLBITS) {
-                    synchronized (this) {
-                        imageLoaded = true;
-                        notify();
-                    }
-                    return true;
-                }
-                return false;
-            }
-        }
-        final StatusObserver imageStatus = new StatusObserver();
-        synchronized (imageStatus) {
-            if (image.getWidth(imageStatus) == -1 || image.getHeight(imageStatus) == -1) {
-                while (!imageStatus.imageLoaded) {
-                    try {
-                        imageStatus.wait();
-                    } catch (InterruptedException ex) {
-                    }
-                }
-            }
-        }
-    }
+	/*
+	 * public byte[] createThumbnailFromDoc(byte[] bytes) throws ConnectException { ByteArrayInputStream bais = new ByteArrayInputStream(bytes);
+	 * ByteArrayOutputStream baos = new ByteArrayOutputStream(); // File inputFile = new File("document.doc"); // File outputFile = new
+	 * File("document.pdf"); // connect to an OpenOffice.org instance running on port 8100 OpenOfficeConnection connection = new
+	 * SocketOpenOfficeConnection(8100); connection.connect(); // convert DocumentConverter converter = new OpenOfficeDocumentConverter(connection);
+	 * // converter.convert(inputFile, outputFile); converter.convert(bais, DocumentFormatRegistry., outputStream, outputFormat); // close the
+	 * connection connection.disconnect(); return new byte[0]; }
+	 */
+
+	public byte[] createDocThumbnail() throws IOException {
+		try (InputStream is = ImageManipulationBean.class.getResourceAsStream("document-48x48.png")) {
+			ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+			int nRead;
+			byte[] data = new byte[16384];
+			while ((nRead = is.read(data, 0, data.length)) != -1) {
+				buffer.write(data, 0, nRead);
+			}
+			buffer.flush();
+			return buffer.toByteArray();
+		}
+	}
 }
--- mgis-web/src/main/java/ru/sovzond/mgis2/web/isogd/DocumentContentRESTController.java
+++ mgis-web/src/main/java/ru/sovzond/mgis2/web/isogd/DocumentContentRESTController.java
@@ -1,25 +1,33 @@
 package ru.sovzond.mgis2.web.isogd;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.IOException;
+import java.util.List;
+import java.util.Set;
+
+import javax.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+
 import ru.sovzond.mgis2.isogd.business.classifiers.representation.RepresentationFormatBean;
 import ru.sovzond.mgis2.isogd.business.document.parts.DocumentContentBean;
 import ru.sovzond.mgis2.isogd.classifiers.documents.representation.RepresentationFormat;
 import ru.sovzond.mgis2.isogd.document.DocumentContent;
 import ru.sovzond.mgis2.preview.ImageManipulationBean;
 
-import javax.transaction.Transactional;
-import java.io.IOException;
-import java.util.List;
-import java.util.Set;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 /**
  * Created by Alexander Arakelyan on 15.07.15.
@@ -29,121 +37,120 @@ import java.util.Set;
 @Scope("session")
 public class DocumentContentRESTController {
 
-    public static final String JPEG = "jpeg";
-    public static final String JPG = "jpg";
-    public static final String PNG = "png";
-    public static final String GIF = "gif";
-    public static final String DOC = "doc";
-    public static final String PDF = "pdf";
-    public static final String UPLOAD_CHARSET = "iso8859-1";
+	public static final String JPEG = "jpeg";
+	public static final String JPG = "jpg";
+	public static final String PNG = "png";
+	public static final String GIF = "gif";
+	public static final String DOC = "doc";
+	public static final String PDF = "pdf";
+	public static final String UPLOAD_CHARSET = "iso8859-1";
 
-    @Autowired
-    private DocumentContentBean documentContentBean;
+	@Autowired
+	private DocumentContentBean documentContentBean;
 
-    @Autowired
-    private RepresentationFormatBean representationFormatBean;
+	@Autowired
+	private RepresentationFormatBean representationFormatBean;
 
-    @Autowired
-    private ImageManipulationBean imageManipulationBean;
+	@Autowired
+	private ImageManipulationBean imageManipulationBean;
 
+	@RequestMapping(value = "/upload", headers = "Accept=*/*", produces = "application/json", method = RequestMethod.POST)
+	@Transactional
+	@ResponseBody
+	public String uploadCommonContent(@RequestBody MultipartFile file) throws IOException {
+		String contentType = file.getContentType();
+		List<RepresentationFormat> list = representationFormatBean.findByFormat(contentType);
+		switch (list.size()) {
+		case 0:
+			throw new IllegalArgumentException("NO_REPRESENTATION_FORMAT_FOUND: " + contentType);
+		case 1:
+			RepresentationFormat representationFormat = list.get(0);
+			DocumentContent documentContent = new DocumentContent();
+			documentContent.setRepresentationFormat(representationFormat);
+			documentContent.setFileName(new String(file.getOriginalFilename().getBytes(UPLOAD_CHARSET)));
+			try {
+				documentContent.setBytes(file.getBytes());
+			} catch (IOException ex) {
+				throw ex;
+			}
+			documentContentBean.save(documentContent);
+			ObjectMapper mapper = new ObjectMapper();
+			try {
+				DocumentContent clone = documentContent.clone();
+				clone.setFileName(file.getOriginalFilename());
+				return mapper.writeValueAsString(clone);
+			} catch (JsonProcessingException ex) {
+				throw ex;
+			}
+		default:
+			throw new IllegalArgumentException("MORE_THAN_ONE_REPRESENTATION_FORMATS_FOUND: " + contentType);
+		}
+	}
 
-    @RequestMapping(value = "/upload", headers = "Accept=*/*", produces = "application/json", method = RequestMethod.POST)
-    @Transactional
-    @ResponseBody
-    public String uploadCommonContent(@RequestBody MultipartFile file) throws IOException {
-        String contentType = file.getContentType();
-        List<RepresentationFormat> list = representationFormatBean.findByFormat(contentType);
-        switch (list.size()) {
-            case 0:
-                throw new IllegalArgumentException("NO_REPRESENTATION_FORMAT_FOUND: " + contentType);
-            case 1:
-                RepresentationFormat representationFormat = list.get(0);
-                DocumentContent documentContent = new DocumentContent();
-                documentContent.setRepresentationFormat(representationFormat);
-                documentContent.setFileName(new String(file.getOriginalFilename().getBytes(UPLOAD_CHARSET)));
-                try {
-                    documentContent.setBytes(file.getBytes());
-                } catch (IOException ex) {
-                    throw ex;
-                }
-                documentContentBean.save(documentContent);
-                ObjectMapper mapper = new ObjectMapper();
-                try {
-                    DocumentContent clone = documentContent.clone();
-                    clone.setFileName(file.getOriginalFilename());
-                    return mapper.writeValueAsString(clone);
-                } catch (JsonProcessingException ex) {
-                    throw ex;
-                }
-            default:
-                throw new IllegalArgumentException("MORE_THAN_ONE_REPRESENTATION_FORMATS_FOUND: " + contentType);
-        }
-    }
+	@RequestMapping(value = "/{contentId}/preview")
+	@Transactional
+	public ResponseEntity<byte[]> preview(@PathVariable("contentId") Long contentId) throws IOException {
+		DocumentContent documentContent = documentContentBean.load(contentId);
+		Set<String> formats = documentContent.getRepresentationFormat().getFormats();
+		MediaType defaultFormat = MediaType.IMAGE_PNG;
+		byte[] previewBytes = null;
+		for (String format : formats) {
+			String format2 = format.toLowerCase();
+			if (format2.contains(JPEG) || format2.contains(JPG)) {
+				defaultFormat = MediaType.IMAGE_JPEG;
+				previewBytes = imageManipulationBean.createThumbnailFromImage(documentContent.getBytes());
+				break;
+			} else if (format2.contains(PNG)) {
+				defaultFormat = MediaType.IMAGE_PNG;
+				previewBytes = imageManipulationBean.createThumbnailFromImage(documentContent.getBytes());
+				break;
+			} else if (format2.contains(GIF)) {
+				defaultFormat = MediaType.IMAGE_GIF;
+				previewBytes = imageManipulationBean.createThumbnailFromImage(documentContent.getBytes());
+				break;
+			} else if (format2.contains(DOC)) {
+				defaultFormat = MediaType.IMAGE_PNG;
+				// previewBytes = imageManipulationBean.createThumbnailFromDoc(documentContent.getBytes());
+				previewBytes = imageManipulationBean.createDocThumbnail();
+				break;
+			} else if (format2.contains(PDF)) {
+				defaultFormat = MediaType.IMAGE_PNG;
+				previewBytes = imageManipulationBean.createDocThumbnail();
+				break;
+			}
+		}
+		final HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(defaultFormat);
+		return new ResponseEntity<>(previewBytes, headers, HttpStatus.CREATED);
+	}
 
-    @RequestMapping(value = "/{contentId}/preview")
-    @Transactional
-    public ResponseEntity<byte[]> preview(@PathVariable("contentId") Long contentId) throws IOException {
-        DocumentContent documentContent = documentContentBean.load(contentId);
-        Set<String> formats = documentContent.getRepresentationFormat().getFormats();
-        MediaType defaultFormat = MediaType.IMAGE_PNG;
-        byte[] previewBytes = null;
-        for (String format : formats) {
-            String format2 = format.toLowerCase();
-            if (format2.contains(JPEG) || format2.contains(JPG)) {
-                defaultFormat = MediaType.IMAGE_JPEG;
-                previewBytes = imageManipulationBean.createThumbnail(documentContent.getBytes());
-                break;
-            } else if (format2.contains(PNG)) {
-                defaultFormat = MediaType.IMAGE_PNG;
-                previewBytes = imageManipulationBean.createThumbnail(documentContent.getBytes());
-                break;
-            } else if (format2.contains(GIF)) {
-                defaultFormat = MediaType.IMAGE_GIF;
-                previewBytes = imageManipulationBean.createThumbnail(documentContent.getBytes());
-                break;
-            } else if (format2.contains(DOC)) {
-                defaultFormat = MediaType.IMAGE_PNG;
-                previewBytes = null;
-                break;
-            } else if (format2.contains(PDF)) {
-                defaultFormat = MediaType.IMAGE_PNG;
-                previewBytes = null;
-                break;
-            }
-        }
-        final HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(defaultFormat);
-        return new ResponseEntity<>(previewBytes, headers, HttpStatus.CREATED);
-    }
-
-
-    @RequestMapping(value = "/{contentId}/download")
-    @Transactional
-    public ResponseEntity<byte[]> download(@PathVariable("contentId") Long contentId) {
-        DocumentContent documentContent = documentContentBean.load(contentId);
-        Set<String> formats = documentContent.getRepresentationFormat().getFormats();
-        MediaType defaultFormat = MediaType.IMAGE_PNG;
-        for (String format : formats) {
-            String format2 = format.toLowerCase();
-            if (format2.contains("jpeg") || format2.contains("jpg")) {
-                defaultFormat = MediaType.IMAGE_JPEG;
-                break;
-            } else if (format2.contains("png")) {
-                defaultFormat = MediaType.IMAGE_PNG;
-                break;
-            } else if (format2.contains("gif")) {
-                defaultFormat = MediaType.IMAGE_GIF;
-                break;
-            } else if (format2.contains("doc")) {
-                defaultFormat = MediaType.APPLICATION_OCTET_STREAM;
-                break;
-            } else if (format2.contains("pdf")) {
-                defaultFormat = MediaType.APPLICATION_OCTET_STREAM;
-                break;
-            }
-        }
-        final HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(defaultFormat);
-        return new ResponseEntity<>(documentContent.getBytes(), headers, HttpStatus.CREATED);
-    }
+	@RequestMapping(value = "/{contentId}/download")
+	@Transactional
+	public ResponseEntity<byte[]> download(@PathVariable("contentId") Long contentId) {
+		DocumentContent documentContent = documentContentBean.load(contentId);
+		Set<String> formats = documentContent.getRepresentationFormat().getFormats();
+		MediaType defaultFormat = MediaType.IMAGE_PNG;
+		for (String format : formats) {
+			String format2 = format.toLowerCase();
+			if (format2.contains("jpeg") || format2.contains("jpg")) {
+				defaultFormat = MediaType.IMAGE_JPEG;
+				break;
+			} else if (format2.contains("png")) {
+				defaultFormat = MediaType.IMAGE_PNG;
+				break;
+			} else if (format2.contains("gif")) {
+				defaultFormat = MediaType.IMAGE_GIF;
+				break;
+			} else if (format2.contains("doc")) {
+				defaultFormat = MediaType.APPLICATION_OCTET_STREAM;
+				break;
+			} else if (format2.contains("pdf")) {
+				defaultFormat = MediaType.APPLICATION_OCTET_STREAM;
+				break;
+			}
+		}
+		final HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(defaultFormat);
+		return new ResponseEntity<>(documentContent.getBytes(), headers, HttpStatus.CREATED);
+	}
 }
