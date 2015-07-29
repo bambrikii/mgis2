83f28d850cf047399d1247eb8114fcabf2aa0a6b
Alexander Arakelyan <arakelyan@sovzond.ru>
2015-07-29 09:10:37 +0300

    - Document parts cascade remove.

--- mgis-model/src/main/java/ru/sovzond/mgis2/isogd/document/Document.java
+++ mgis-model/src/main/java/ru/sovzond/mgis2/isogd/document/Document.java
@@ -17,130 +17,130 @@ import java.util.Date;
 @Table(name = "isogd_document")
 public class Document implements Cloneable {
 
-    @Id
-    @SequenceGenerator(name = "pk_sequence", sequenceName = "isogd_entity_seq", allocationSize = 1)
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "pk_sequence")
-    @Column
-    private Long id;
+	@Id
+	@SequenceGenerator(name = "pk_sequence", sequenceName = "isogd_entity_seq", allocationSize = 1)
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "pk_sequence")
+	@Column
+	private Long id;
 
-    @Column
-    private String docNumber;
+	@Column
+	private String docNumber;
 
-    @Column
-    private Date docDate;
+	@Column
+	private Date docDate;
 
-    @Column
-    private String name;
+	@Column
+	private String name;
 
-    @ManyToOne(optional = false)
-    private Volume volume;
+	@ManyToOne(optional = false)
+	private Volume volume;
 
-    @OneToOne(mappedBy = "document")
-    private CommonPart commonPart;
+	@OneToOne(mappedBy = "document", cascade = CascadeType.REMOVE)
+	private CommonPart commonPart;
 
-    @OneToOne(mappedBy = "document")
-    private SpecialPart specialPart;
+	@OneToOne(mappedBy = "document", cascade = CascadeType.REMOVE)
+	private SpecialPart specialPart;
 
-    @ManyToOne
-    private DocumentSubObject documentSubObject;
+	@ManyToOne
+	private DocumentSubObject documentSubObject;
 
-    @ManyToOne
-    private RepresentationFormat representationFormat;
+	@ManyToOne
+	private RepresentationFormat representationFormat;
 
-    @ManyToOne
-    private Territory oktmo;
+	@ManyToOne
+	private Territory oktmo;
 
-    public Long getId() {
-        return id;
-    }
+	public Long getId() {
+		return id;
+	}
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+	public void setId(Long id) {
+		this.id = id;
+	}
 
-    public String getName() {
-        return name;
-    }
+	public String getName() {
+		return name;
+	}
 
-    public void setName(String name) {
-        this.name = name;
-    }
+	public void setName(String name) {
+		this.name = name;
+	}
 
-    public Volume getVolume() {
-        return volume;
-    }
+	public Volume getVolume() {
+		return volume;
+	}
 
-    public void setVolume(Volume volume) {
-        this.volume = volume;
-    }
+	public void setVolume(Volume volume) {
+		this.volume = volume;
+	}
 
-    public String getDocNumber() {
-        return docNumber;
-    }
+	public String getDocNumber() {
+		return docNumber;
+	}
 
-    public void setDocNumber(String docNumber) {
-        this.docNumber = docNumber;
-    }
+	public void setDocNumber(String docNumber) {
+		this.docNumber = docNumber;
+	}
 
-    public Date getDocDate() {
-        return docDate;
-    }
+	public Date getDocDate() {
+		return docDate;
+	}
 
-    public void setDocDate(Date docDate) {
-        this.docDate = docDate;
-    }
+	public void setDocDate(Date docDate) {
+		this.docDate = docDate;
+	}
 
-    public RepresentationFormat getRepresentationFormat() {
-        return representationFormat;
-    }
+	public RepresentationFormat getRepresentationFormat() {
+		return representationFormat;
+	}
 
-    public void setRepresentationFormat(RepresentationFormat representationFormat) {
-        this.representationFormat = representationFormat;
-    }
+	public void setRepresentationFormat(RepresentationFormat representationFormat) {
+		this.representationFormat = representationFormat;
+	}
 
-    public Territory getOktmo() {
-        return oktmo;
-    }
+	public Territory getOktmo() {
+		return oktmo;
+	}
 
-    public void setOktmo(Territory oktmo) {
-        this.oktmo = oktmo;
-    }
+	public void setOktmo(Territory oktmo) {
+		this.oktmo = oktmo;
+	}
 
-    public DocumentSubObject getDocumentSubObject() {
-        return documentSubObject;
-    }
+	public DocumentSubObject getDocumentSubObject() {
+		return documentSubObject;
+	}
 
-    public void setDocumentSubObject(DocumentSubObject documentSubObject) {
-        this.documentSubObject = documentSubObject;
-    }
+	public void setDocumentSubObject(DocumentSubObject documentSubObject) {
+		this.documentSubObject = documentSubObject;
+	}
 
-    public CommonPart getCommonPart() {
-        return commonPart;
-    }
+	public CommonPart getCommonPart() {
+		return commonPart;
+	}
 
-    public void setCommonPart(CommonPart commonPart) {
-        this.commonPart = commonPart;
-    }
+	public void setCommonPart(CommonPart commonPart) {
+		this.commonPart = commonPart;
+	}
 
-    public SpecialPart getSpecialPart() {
-        return specialPart;
-    }
+	public SpecialPart getSpecialPart() {
+		return specialPart;
+	}
 
-    public void setSpecialPart(SpecialPart specialPart) {
-        this.specialPart = specialPart;
-    }
+	public void setSpecialPart(SpecialPart specialPart) {
+		this.specialPart = specialPart;
+	}
 
-    public Document clone() {
-        Document document = new Document();
-        document.setId(id);
-        document.setDocNumber(docNumber);
-        document.setDocDate(docDate);
-        document.setName(name);
-        document.setOktmo(oktmo);
-        document.setCommonPart(commonPart != null ? commonPart.clone() : null);
-        document.setSpecialPart(specialPart != null ? specialPart.clone() : null);
-        document.setDocumentSubObject(documentSubObject != null ? documentSubObject.clone() : null);
-        document.setVolume(volume.clone());
-        return document;
-    }
+	public Document clone() {
+		Document document = new Document();
+		document.setId(id);
+		document.setDocNumber(docNumber);
+		document.setDocDate(docDate);
+		document.setName(name);
+		document.setOktmo(oktmo);
+		document.setCommonPart(commonPart != null ? commonPart.clone() : null);
+		document.setSpecialPart(specialPart != null ? specialPart.clone() : null);
+		document.setDocumentSubObject(documentSubObject != null ? documentSubObject.clone() : null);
+		document.setVolume(volume.clone());
+		return document;
+	}
 }
