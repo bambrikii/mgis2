ca50a01099a2bb3e0caf18f0e4475fd2d8931440
Alexander Arakelyan <arakelyan@sovzond.ru>
2015-08-21 17:47:25 +0300

    - projections.

--- mgis-web/src/main/webapp/app2/lands/land/land-maps-module.js
+++ mgis-web/src/main/webapp/app2/lands/land/land-maps-module.js
@@ -1,130 +1,177 @@
 angular.module("mgis.lands.maps", ["ui.router", "ui.bootstrap", "ui.select", "openlayers-directive", //
 //	"mgis.commons", //
-    "mgis.lands.services",
+	"mgis.lands.services",
 //	"mgis.nc.services",
 //	"mgis.common.executive_person.service",
 //	"mgis.terr-zones.zone.service"
 ])
-    .config(function ($stateProvider) {
-        $stateProvider
-            .state("lands.maps", {
-                url: "/maps/",
-                templateUrl: "app2/lands/land/land-maps.htm"
-            });
-    })
+	.config(function ($stateProvider) {
+		$stateProvider
+			.state("lands.maps", {
+				url: "/maps/",
+				templateUrl: "app2/lands/land/land-maps.htm"
+			});
+	})
 //
-    .controller("LandsMapsController", function ($scope, $state, LandsLandService) {
+	.controller("LandsMapsController", function ($scope, $state, LandsLandService) {
 
-        ol.ProxyHost = "/proxy.jsp?url=";
+		ol.ProxyHost = "/proxy.jsp?url=";
 
-        $scope.displayOnTheMap = function () {
-            $state.go("^.lands");
-        }
+		$scope.displayOnTheMap = function () {
+			$state.go("^.lands");
+		}
 
-        var landsVectorSource = new ol.source.ServerVector({
-            format: new ol.format.GeoJSON(),
-            loader: function (extent, resolution, projection) {
-                var url = 'proxy?' + ('http://localhost:8081/geoserver/mgis2/wfs?service=WFS&version=1.1.0&layers=mgis2:lands_land&request=GetFeature&typeNames=lands_land&srsName=EPSG:3857&bbox=' + extent.join(','));
-                $.ajax({
-                    url: url,
-                    success: function (data) {
-                        landsVectorSource.addFeatures(landsVectorSource.readFeatures(data));
-                    },
-                    error: function () {
-                        console.log("error:");
-                    }
-                });
-            },
-            projection: 'EPSG:3857',
-            strategy: ol.loadingstrategy.createTile(new ol.tilegrid.XYZ({
-                maxZoom: 19
-            }))
-        });
+		var projection2 = ol.proj.get('EPSG:3857');
 
-        //
-        var map = new ol.Map({
-            target: 'map',
-            layers: [
-                new ol.layer.Group({
-                    'title': 'Base maps',
-                    layers: [
-                        new ol.layer.Tile({
-                            title: 'Water color',
-                            type: 'base',
-                            visible: false,
-                            source: new ol.source.Stamen({
-                                layer: 'watercolor'
-                            })
-                        }),
-                        new ol.layer.Tile({
-                            title: 'OSM',
-                            type: 'base',
-                            visible: true,
-                            source: new ol.source.OSM()
-                        }),
-                        new ol.layer.Tile({
-                            title: 'Satellite',
-                            type: 'base',
-                            visible: false,
-                            source: new ol.source.MapQuest({layer: 'sat'})
-                        })
-                    ]
-                }),
-                new ol.layer.Group({
-                    title: 'Overlays',
-                    layers: [
-                        new ol.layer.Tile({
-                            title: 'Countries',
-                            source: new ol.source.TileWMS({
-                                url: 'http://demo.opengeo.org/geoserver/wms',
-                                params: {'LAYERS': 'ne:ne_10m_admin_1_states_provinces_lines_shp'},
-                                serverType: 'geoserver'
-                            })
-                        })
-                    ]
-                }),
-                new ol.layer.Group({
-                    title: 'MGIS2',
-                    layers: [
-                        new ol.layer.Tile({
-                            title: 'Lands',
-                            source: new ol.source.TileWMS({
-                                url: 'http://demo.opengeo.org/geoserver/wms',
-                                params: {'LAYERS': 'ne:ne_10m_admin_1_states_provinces_lines_shp'},
-                                serverType: 'geoserver'
-                            })
-                        }),
-                        new ol.layer.Vector({
-                            title: 'Lands Vector',
-                            source: landsVectorSource,
-                            style: new ol.style.Style({
-                                image: new ol.style.Circle({
-                                    radius: 3,
-                                    fill: new ol.style.Fill({color: 'white'})
-                                })
-                            })
-                        })
-                    ]
-                })
-            ],
-            view: new ol.View({
-                center: ol.proj.transform([-0.92, 52.96], 'EPSG:4326', 'EPSG:3857'),
-                zoom: 6
-            })
-        });
+		var landsVectorSource = new ol.source.ServerVector({
+			format: new ol.format.GeoJSON(),
+			loader: function (extent, resolution, projection) {
+				console.log(extent);
+				console.log(JSON.stringify(projection));
+				var url = 'proxy?' + ('http://localhost:8081/geoserver/mgis2/wfs?service=WFS&version=1.1.0&layers=mgis2:lands_land&request=GetFeature&typeNames=lands_land&bbox=' + extent.join(',') + ',' + projection.a + '&outputFormat=application/json&srsName=' + /*projection.a*/ "EPSG:3857"/* "EPSG:41001"*/);
+				$.ajax({
+					url: url,
+					success: function (data) {
+						var features = landsVectorSource.readFeatures(data);
+						console.log(features.length);
+						landsVectorSource.addFeatures(landsVectorSource.readFeatures(data));
+					},
+					error: function (data) {
+						console.log("error: " + JSON.stringify(data));
+					}
+				});
+			},
+			//projection: 'EPSG:4326',
+			//projection: projection2,
+			strategy: ol.loadingstrategy.createTile(new ol.tilegrid.XYZ({
+				maxZoom: 19
+			}))
+		});
 
-        var layerSwitcher = new ol.control.LayerSwitcher({
-            //tipLabel: 'Légende' // Optional label for button
-        });
-        map.addControl(layerSwitcher);
+		var mousePositionControl = new ol.control.MousePosition({
+			coordinateFormat: ol.coordinate.createStringXY(4),
+			//projection: 'EPSG:3857',
+			// comment the following two lines to have the mouse position
+			// be placed within the map.
+			className: 'custom-mouse-position',
+			target: document.getElementById('mouse-position'),
+			undefinedHTML: '&nbsp;'
+		});
 
-        $scope.find = function () {
-            LandsLandService.get("", $scope.first, $scope.max, $scope.cadastralNumber).then(function (data) {
-                $scope.list = data.list;
-                $scope.first = data.first;
-                $scope.max = data.max;
-            });
-        }
+		//
+		var map = new ol.Map({
+			target: 'map',
+			numZoomLevels: 20,
+			//projection: new ol.proj.Projection({
+			//	code: 'EPSG:4326',
+			//	units: 'm',
+			//	extent: [-180.0000, -90.0000, 180.0000, 90.0000]
+			//}),
+			//displayProjection: new ol.proj.Projection({
+			//	code: 'EPSG:4326',
+			//	units: 'm',
+			//	extent: [-180.0000, -90.0000, 180.0000, 90.0000]
+			//}),
+			//projection: "EPSG:3857",
+			//displayProjection: "EPSG:3857",
+			layers: [
+				//new ol.layer.Group({
+				//	'title': 'Base maps',
+				//	layers: [
+				//new ol.layer.Tile({
+				//	title: 'Water color',
+				//	type: 'base',
+				//	visible: false,
+				//	source: new ol.source.Stamen({
+				//		layer: 'watercolor'
+				//	})
+				//}),
+				new ol.layer.Tile({
+					title: 'OSM',
+					type: 'base',
+					visible: true,
+					source: new ol.source.OSM()
+				})
+				//,
+				//new ol.layer.Tile({
+				//	title: 'Satellite',
+				//	type: 'base',
+				//	visible: false,
+				//	source: new ol.source.MapQuest({layer: 'sat'})
+				//})
+				//]
+				//})
+				,
+				//new ol.layer.Group({
+				//	title: 'Overlays',
+				//	layers: [
+				//		new ol.layer.Tile({
+				//			title: 'Countries',
+				//			source: new ol.source.TileWMS({
+				//				url: 'http://demo.opengeo.org/geoserver/wms',
+				//				params: {'LAYERS': 'ne:ne_10m_admin_1_states_provinces_lines_shp'},
+				//				serverType: 'geoserver'
+				//			})
+				//		})
+				//	]
+				//}),
+				//new ol.layer.Group({
+				//	title: 'MGIS2',
+				//	layers: [
+				//new ol.layer.Tile({
+				//	title: 'Lands',
+				//	source: new ol.source.TileWMS({
+				//		url: 'http://demo.opengeo.org/geoserver/wms',
+				//		params: {'LAYERS': 'ne:ne_10m_admin_1_states_provinces_lines_shp'},
+				//		serverType: 'geoserver'
+				//	})
+				//}),
+				new ol.layer.Vector({
+					title: 'Lands Vector',
+					source: landsVectorSource,
+					//projection: 'EPSG:3857',
+					projection: projection2,
+					//projection: "EPSG:4326",
+					style: new ol.style.Style({
+						stroke: new ol.style.Stroke({
+							color: 'blue',
+							width: 3
+						}),
+						fill: new ol.style.Fill({
+							color: 'rgba(0, 0, 255, 0.1)'
+						})
+					})
+				})
+				//]
+				//})
+			],
+			view: new ol.View({
+				//projection: projection2,
+				center: ol.proj.transform([/*41.9833, 45.0500*/ 0, 0], 'EPSG:4326', 'EPSG:3857'), //ol.proj.transform([-0.92, 52.96], 'EPSG:4326', 'EPSG:3857'),
+				zoom: 1
+			}),
+			controls: ol.control.defaults({
+				attributionOptions: /** @type {olx.control.AttributionOptions} */ ({
+					collapsible: true
+				})
+			}).extend([new ol.control.ScaleLine({
+				units: 'metric'
+			}), mousePositionControl]),
+		});
+		console.log(map);
 
-    })
+		var layerSwitcher = new ol.control.LayerSwitcher({
+			//tipLabel: 'Légende' // Optional label for button
+		});
+		map.addControl(layerSwitcher);
+
+		$scope.find = function () {
+			LandsLandService.get("", $scope.first, $scope.max, $scope.cadastralNumber).then(function (data) {
+				$scope.list = data.list;
+				$scope.first = data.first;
+				$scope.max = data.max;
+			});
+		}
+
+	})
 ;
