05e40e82a6ddd7b0ba427f50d343a31df70827c3
Alexander Arakelyan <arakelyan@sovzond.ru>
2015-09-03 16:15:32 +0300

    - resource files for application server.

--- mgis-web/src/main/java/ru/sovzond/mgis2/web/ApplicationConfiguration.java
+++ mgis-web/src/main/java/ru/sovzond/mgis2/web/ApplicationConfiguration.java
@@ -18,6 +18,7 @@ import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.ContentNegotiatingViewResolver;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
@@ -36,121 +37,128 @@ import java.util.Properties;
 @PropertySource(value = {"classpath:application.properties"})
 public class ApplicationConfiguration extends WebMvcConfigurerAdapter {
 
-    @Autowired
-    private Environment environment;
-
-    /*
-     * Configure ContentNegotiationManager
-     */
-    @Override
-    public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
-        configurer.ignoreAcceptHeader(true).defaultContentType(MediaType.TEXT_HTML);
-    }
-
-    @Bean
-    public LocalSessionFactoryBean sessionFactory() {
-        LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
-        sessionFactory.setDataSource(dataSource());
-        sessionFactory.setPackagesToScan(new String[]{"ru.sovzond.mgis2"});
-        sessionFactory.setHibernateProperties(hibernateProperties());
-        return sessionFactory;
-    }
-
-    @Bean
-    public DataSource dataSource() {
-        DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName(environment.getRequiredProperty("jdbc.driverClassName"));
-        dataSource.setUrl(environment.getRequiredProperty("jdbc.url"));
-        dataSource.setUsername(environment.getRequiredProperty("jdbc.username"));
-        dataSource.setPassword(environment.getRequiredProperty("jdbc.password"));
-        return dataSource;
-    }
-
-    private Properties hibernateProperties() {
-        Properties properties = new Properties();
-        properties.put("hibernate.dialect", environment.getRequiredProperty("hibernate.dialect"));
-        properties.put("hibernate.show_sql", environment.getRequiredProperty("hibernate.show_sql"));
-        properties.put("hibernate.format_sql", environment.getRequiredProperty("hibernate.format_sql"));
-        properties.put("hibernate.hbm2ddl.auto", environment.getRequiredProperty("hibernate.hbm2ddl.auto"));
-        return properties;
-    }
-
-    @Bean
-    @Autowired
-    public HibernateTransactionManager transactionManager(SessionFactory s) {
-        HibernateTransactionManager txManager = new HibernateTransactionManager();
-        txManager.setSessionFactory(s);
-        return txManager;
-    }
-
-    @Bean
-    public ViewResolver contentNegotiatingViewResolver(ContentNegotiationManager manager) {
-        ContentNegotiatingViewResolver resolver = new ContentNegotiatingViewResolver();
-        resolver.setContentNegotiationManager(manager);
-
-        // Define all possible view resolvers
-        List<ViewResolver> resolvers = new ArrayList<ViewResolver>();
-
-        // resolvers.add(jaxb2MarshallingXmlViewResolver());
-        resolvers.add(jsonViewResolver());
-        resolvers.add(jspViewResolver());
-        // resolvers.add(pdfViewResolver());
-        // resolvers.add(excelViewResolver());
-
-        resolver.setViewResolvers(resolvers);
-        return resolver;
-    }
+	@Autowired
+	private Environment environment;
 
 	/*
-     * Configure View resolver to provide XML output Uses JAXB2 marshaller to marshall/unmarshall POJO's (with JAXB annotations) to XML
+	 * Configure ContentNegotiationManager
 	 */
-    // @Bean
-    // public ViewResolver jaxb2MarshallingXmlViewResolver() {
-    // Jaxb2Marshaller marshaller = new Jaxb2Marshaller();
-    // // marshaller.setClassesToBeBound(Pizza.class);
-    // return new Jaxb2MarshallingXmlViewResolver(marshaller);
-    // }
+	@Override
+	public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+		configurer.ignoreAcceptHeader(true).defaultContentType(MediaType.TEXT_HTML);
+	}
 
-    /*
-     * Configure View resolver to provide JSON output using JACKSON library to convert object in JSON format.
-     */
-    @Bean
-    public ViewResolver jsonViewResolver() {
-        return new JsonViewResolver();
-    }
+	@Bean
+	public LocalSessionFactoryBean sessionFactory() {
+		LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
+		sessionFactory.setDataSource(dataSource());
+		sessionFactory.setPackagesToScan(new String[]{"ru.sovzond.mgis2"});
+		sessionFactory.setHibernateProperties(hibernateProperties());
+		return sessionFactory;
+	}
+
+	@Bean
+	public DataSource dataSource() {
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName(environment.getRequiredProperty("jdbc.driverClassName"));
+		dataSource.setUrl(environment.getRequiredProperty("jdbc.url"));
+		dataSource.setUsername(environment.getRequiredProperty("jdbc.username"));
+		dataSource.setPassword(environment.getRequiredProperty("jdbc.password"));
+		return dataSource;
+	}
+
+	private Properties hibernateProperties() {
+		Properties properties = new Properties();
+		properties.put("hibernate.dialect", environment.getRequiredProperty("hibernate.dialect"));
+		properties.put("hibernate.show_sql", environment.getRequiredProperty("hibernate.show_sql"));
+		properties.put("hibernate.format_sql", environment.getRequiredProperty("hibernate.format_sql"));
+		properties.put("hibernate.hbm2ddl.auto", environment.getRequiredProperty("hibernate.hbm2ddl.auto"));
+		return properties;
+	}
+
+	@Bean
+	@Autowired
+	public HibernateTransactionManager transactionManager(SessionFactory s) {
+		HibernateTransactionManager txManager = new HibernateTransactionManager();
+		txManager.setSessionFactory(s);
+		return txManager;
+	}
+
+	@Bean
+	public ViewResolver contentNegotiatingViewResolver(ContentNegotiationManager manager) {
+		ContentNegotiatingViewResolver resolver = new ContentNegotiatingViewResolver();
+		resolver.setContentNegotiationManager(manager);
+
+		// Define all possible view resolvers
+		List<ViewResolver> resolvers = new ArrayList<ViewResolver>();
+
+		// resolvers.add(jaxb2MarshallingXmlViewResolver());
+		resolvers.add(jsonViewResolver());
+		resolvers.add(jspViewResolver());
+		// resolvers.add(pdfViewResolver());
+		// resolvers.add(excelViewResolver());
+
+		resolver.setViewResolvers(resolvers);
+		return resolver;
+	}
 
 	/*
-     * Configure View resolver to provide PDF output using lowagie pdf library to generate PDF output for an object content
+	 * Configure View resolver to provide XML output Uses JAXB2 marshaller to marshall/unmarshall POJO's (with JAXB annotations) to XML
 	 */
-    // @Bean
-    // public ViewResolver pdfViewResolver() {
-    // return new PdfViewResolver();
-    // }
+	// @Bean
+	// public ViewResolver jaxb2MarshallingXmlViewResolver() {
+	// Jaxb2Marshaller marshaller = new Jaxb2Marshaller();
+	// // marshaller.setClassesToBeBound(Pizza.class);
+	// return new Jaxb2MarshallingXmlViewResolver(marshaller);
+	// }
 
 	/*
-     * Configure View resolver to provide XLS output using Apache POI library to generate XLS output for an object content
+	 * Configure View resolver to provide JSON output using JACKSON library to convert object in JSON format.
 	 */
-    // @Bean
-    // public ViewResolver excelViewResolver() {
-    // return new ExcelViewResolver();
-    // }
+	@Bean
+	public ViewResolver jsonViewResolver() {
+		return new JsonViewResolver();
+	}
 
-    /*
-     * Configure View resolver to provide HTML output This is the default format in absence of any type suffix.
-     */
-    @Bean
-    public ViewResolver jspViewResolver() {
-        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
-        viewResolver.setViewClass(JstlView.class);
-        viewResolver.setPrefix("/WEB-INF/pages/");
-        viewResolver.setSuffix(".jsp");
-        return viewResolver;
-    }
+	/*
+	 * Configure View resolver to provide PDF output using lowagie pdf library to generate PDF output for an object content
+	 */
+	// @Bean
+	// public ViewResolver pdfViewResolver() {
+	// return new PdfViewResolver();
+	// }
 
-    @Bean
-    public MultipartResolver multipartResolver() {
-        CommonsMultipartResolver multipartResolver = new CommonsMultipartResolver();
-        multipartResolver.setMaxUploadSize(500000);
-        return multipartResolver;
-    }
+	/*
+	 * Configure View resolver to provide XLS output using Apache POI library to generate XLS output for an object content
+	 */
+	// @Bean
+	// public ViewResolver excelViewResolver() {
+	// return new ExcelViewResolver();
+	// }
+
+	/*
+	 * Configure View resolver to provide HTML output This is the default format in absence of any type suffix.
+	 */
+	@Bean
+	public ViewResolver jspViewResolver() {
+		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+		viewResolver.setViewClass(JstlView.class);
+		viewResolver.setPrefix("/WEB-INF/pages/");
+		viewResolver.setSuffix(".jsp");
+		return viewResolver;
+	}
+
+	@Bean
+	public MultipartResolver multipartResolver() {
+		CommonsMultipartResolver multipartResolver = new CommonsMultipartResolver();
+		multipartResolver.setMaxUploadSize(500000);
+		return multipartResolver;
+	}
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/app2/**").addResourceLocations("/app2/");
+		registry.addResourceHandler("/template/**").addResourceLocations("/template/");
+		registry.addResourceHandler("/bower_components/**").addResourceLocations("/bower_components/");
+	}
 }
