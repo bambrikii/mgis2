6aa688f99c3c91607b97c0069c169d1eb98d98dd
Ալեքսանդր Առաքելյան <bambrikii@gmail.com>
2015-09-10 06:47:44 +0300

    - KLADR.

--- mgis-initial-data/KLADR/convert-all.sh
+++ mgis-initial-data/KLADR/convert-all.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-wget http://www.vsepro1c.ru/files/download/KLADR_20.08.2015.7z
+if [ ! -f ./KLADR_20.08.2015.7z ]; then
+    wget http://www.vsepro1c.ru/files/download/KLADR_20.08.2015.7z
+fi
 #sudo apt-get install p7zip-full
+#sudo apt-get install dbview
+
 7z x -y KLADR_20.08.2015.7z
 
 ./convert-one.sh KLADR
