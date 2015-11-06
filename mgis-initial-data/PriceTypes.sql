716ce24fa64c429c59f02edc080e107500300bdd
Alexander Arakelyan <arakelyan@sovzond.ru>
2015-11-06 17:52:41 +0300

    - mgis2_property_rights.

---
+++ mgis-initial-data/PriceTypes.sql
@@ -0,0 +1,6 @@
+INSERT INTO t1 (id, name) VALUES (nextval('_seq'), 'Балансовая стоимость');
+INSERT INTO t1 (id, name) VALUES (nextval('_seq'), 'Остаточная стоимость');
+INSERT INTO t1 (id, name) VALUES (nextval('_seq'), 'Действительная инвентаризационная стоимость');
+INSERT INTO t1 (id, name) VALUES (nextval('_seq'), 'Восстановительная стоимость');
+INSERT INTO t1 (id, name) VALUES (nextval('_seq'), 'Кадастровая стоимость');
+INSERT INTO t1 (id, name) VALUES (nextval('_seq'), 'Рыночная стоимость');
