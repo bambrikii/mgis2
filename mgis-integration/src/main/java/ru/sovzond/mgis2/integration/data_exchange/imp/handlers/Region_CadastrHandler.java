e41ecdffe20cc3846c767c27755b13b2812e967f
Alexander Arakelyan <arakelyan@sovzond.ru>
2015-11-19 09:10:26 +0300

    - rusregister xml nodes .

--- mgis-integration/src/main/java/ru/sovzond/mgis2/integration/data_exchange/imp/handlers/Region_CadastrHandler.java
+++ mgis-integration/src/main/java/ru/sovzond/mgis2/integration/data_exchange/imp/handlers/Region_CadastrHandler.java
@@ -12,31 +12,67 @@ import org.xml.sax.helpers.DefaultHandler;
  * - Package
  * -  Cadastral_Blocks
  * -   Cadastral_Block
- * -   Area
- * -   Parcels
- * -    Parcel[CadastralNumber,Name,State,DateCreated]
- * -     Area
+ * -    Area
+ * -    Parcels
+ * -     Parcel[CadastralNumber,Name,State,DateCreated]
  * -      Area
- * -      Unit
- * -     Location
- * -      inBounds
- * -      Placed
- * -      Address
- * -       Code_OKATO
- * -       Code_KLADR
- * -       Region
- * -       District[Name,Type]
- * -       Locality[Name,Type]
- * -       Street[Name,Type]
- * -       Level1
- * -       Note
- * -   OMSPoints
- * -
- * -   SpatialData
- * -
- * -   Bounds
- * -
- * -   Certification_Doc
+ * -       Area
+ * -       Unit
+ * -      Location
+ * -       inBounds
+ * -       Placed
+ * -       Address
+ * -        Code_OKATO
+ * -        Code_KLADR
+ * -        Region
+ * -        District[Name,Type]
+ * -        Locality[Name,Type]
+ * -        Street[Name,Type]
+ * -        Level1
+ * -        Note
+ * -    OMSPoints
+ * -     OMSPoint
+ * -      PNmb
+ * -      PName
+ * -      PKlass
+ * -      OrdX
+ * -      OrdY
+ * -    SpatialData
+ * -     Entity_Spatial[Ent_Sys]
+ * -      Spatial_Element
+ * -       Spelement_Unit[Type_Unit,Su_Nmb]
+ * -        Ordinate[X,Y,Ord_Nmb]
+ * -     Documents
+ * -      Document
+ * -       Code_Document
+ * -       Name
+ * -       Number
+ * -       IssueOrgan
+ * -    Bounds
+ * -     Bound
+ * -      AccountNumber
+ * -      Boundaries
+ * -       Boundary
+ * -        Entity_Spatial[Ent_Sys]
+ * -         Spatial_Element
+ * -          Spelement_Unit[Type_Unit,Su_Nmb]
+ * -           Ordinate[X,Y,Ord_Nmb,Geopoint_Opred,Delta_Geopoint]
+ * -      Documents
+ * -       Document
+ * -        Code_Document
+ * -        Name
+ * -        Number
+ * -        Date
+ * -        IssueOrgan
+ * -      InhabitedLocalityBoundary
+ * -       Name
+ * -    Coord_System[Name,Cs_Id]
+ * -  Certification_Doc
+ * -   Organization
+ * -   Date
+ * -   Number
+ * -   Appointment
+ * -   FIO
  * -
  */
 public class Region_CadastrHandler extends DefaultHandler {
