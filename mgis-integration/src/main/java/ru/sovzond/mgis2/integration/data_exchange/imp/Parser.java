b3d4cd29224e519d75f5bc41e7de474c784ce549
donchenko-y <y.donchenko@sovzond.ru>
2015-11-20 11:03:58 +0300

    add tags CadastrHandler

---
+++ mgis-integration/src/main/java/ru/sovzond/mgis2/integration/data_exchange/imp/Parser.java
@@ -0,0 +1,39 @@
+package ru.sovzond.mgis2.integration.data_exchange.imp;
+
+/**
+ * Created by donchenko-y on 11/18/15.
+ */
+
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class Parser {
+
+	SAXParserFactory factory = SAXParserFactory.newInstance();
+	SAXParser parser = null;
+
+	public Parser() {
+		try {
+			parser = factory.newSAXParser();
+		} catch (ParserConfigurationException | SAXException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void parse(File xmlFile, DefaultHandler handler) throws SAXException, IOException {
+		parser.parse(xmlFile, handler);
+	}
+
+	public void parse(InputStream inputStream, DefaultHandler handler) throws SAXException, IOException {
+		parser.parse(inputStream, handler);
+	}
+
+
+}
