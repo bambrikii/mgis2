ea4b2900b88da0bcac24d7db55fd45cbd56350f3
donchenko-y <y.donchenko@sovzond.ru>
2015-11-20 11:03:33 +0300

    add tags CadastrHandler

--- mgis-integration/src/main/java/ru/sovzond/mgis2/integration/data_exchange/imp/handlers/Region_CadastrHandler.java
+++ mgis-integration/src/main/java/ru/sovzond/mgis2/integration/data_exchange/imp/handlers/Region_CadastrHandler.java
@@ -4,10 +4,11 @@ import org.xml.sax.Attributes;
 import org.xml.sax.SAXException;
 import org.xml.sax.SAXParseException;
 import org.xml.sax.helpers.DefaultHandler;
+import ru.sovzond.mgis2.lands.Land;
 
 /**
  * Created by Alexander Arakelyan on 18.11.15.
- * <p>
+ * <p/>
  * Region
  * - Package
  * -  Cadastral_Blocks
@@ -76,9 +77,67 @@ import org.xml.sax.helpers.DefaultHandler;
  * -
  */
 public class Region_CadastrHandler extends DefaultHandler {
+
+	/**
+	 * ********************************************************
+	 * тэги файла xml false - закрытый тэг , true - открытый
+	 * ********************************************************
+	 */
+
+	//-------Parcel attributes name-----
+	final static String CADASTRAL_NUMBER = "CadastralNumber";
+	final static String NAME = "Name";
+	final static String STATE = "State";
+	final static String DATE_CR = "DateCreated";
+	//-------INIT-----------------------
+	final static String TOTAL = "Total";
+	final static String UNIT = "Unit";
+
+	boolean t_AreaM = false;//-----Area all
+	//--------Parcels-----------
+	boolean t_Parcels = false;
+	boolean t_Parcel = false;
+	boolean t_Area = false;//-----Area
+	boolean t_AreaIn = false;
+	boolean t_Unit = false;
+	//--------------------------
+	boolean t_Location = false;//-----Location
+	boolean t_inBounds = false;
+	boolean t_Placed = false;
+	//---------------------------
+	boolean t_Code_OKATO = false;//-----Address (In Location)
+	boolean t_Code_KLADR = false;
+	boolean t_Region = false;
+	boolean t_District = false;
+	boolean t_Locality = false;
+	boolean t_Street = false;
+	boolean t_Note = false;
+	//---------------------------
+	boolean t_Category = false;
+	boolean t_Utilization = false;
+	//---------------------------
+	boolean t_Rights = false;//-----Rights
+	boolean t_Right = false;
+	boolean t_Name = false;
+	boolean t_Type = false;
+	//---------------------------
+	boolean t_CadastralCost = false;
+	//-------OMS_Points-----------
+	boolean t_OMSPoints = false;
+	boolean t_OMSPoint = false;
+	boolean t_PNmb = false;
+	boolean t_PName = false;
+	boolean t_PKlass = false;
+	boolean t_OrdX = false;
+	boolean t_OrdY = false;
+
+
+
+
 	@Override
 	public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
 		super.startElement(uri, localName, qName, attributes);
+
 	}
 
 	@Override
