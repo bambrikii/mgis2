7fef66c1e02371e38ad2b214fa4ff027da6091c8
donchenko-y <y.donchenko@sovzond.ru>
2015-11-26 11:26:34 +0300

    MGIS-12
    Merge remote-tracking branch 'origin/branches/0.1.9-import' into branches/0.1.9-import
    
    # Conflicts:
    #	mgis-integration/src/main/java/ru/sovzond/mgis2/integration/data_exchange/imp/dto/EntitySpatialDTO.java
    #	mgis-integration/src/main/java/ru/sovzond/mgis2/integration/data_exchange/imp/dto/LandDTO.java
    #	mgis-integration/src/main/java/ru/sovzond/mgis2/integration/data_exchange/imp/dto/LandRightDTO.java
    #	mgis-integration/src/main/java/ru/sovzond/mgis2/integration/data_exchange/imp/dto/OrdinateDTO.java
    #	mgis-integration/src/main/java/ru/sovzond/mgis2/integration/data_exchange/imp/dto/SpatialElementDTO.java
    #	mgis-integration/src/main/java/ru/sovzond/mgis2/integration/data_exchange/imp/dto/SpatialElementUnitDTO.java

--- mgis-integration/src/main/java/ru/sovzond/mgis2/integration/data_exchange/imp/handlers/Region_CadastrHandler.java
+++ mgis-integration/src/main/java/ru/sovzond/mgis2/integration/data_exchange/imp/handlers/Region_CadastrHandler.java
@@ -4,6 +4,8 @@ import org.xml.sax.Attributes;
 import org.xml.sax.SAXException;
 import org.xml.sax.SAXParseException;
 import org.xml.sax.helpers.DefaultHandler;
+import ru.sovzond.mgis2.integration.data_exchange.imp.LandsImporter;
+import ru.sovzond.mgis2.integration.data_exchange.imp.dto.*;
 import ru.sovzond.mgis2.lands.Land;
 
 /**
@@ -78,9 +80,19 @@ import ru.sovzond.mgis2.lands.Land;
  */
 public class Region_CadastrHandler extends DefaultHandler {
 
+	AddressDTO addressDTO;
+	EntitySpatialDTO entitySpatialDTO;
+	LandDTO landDTO;
+	LandRightDTO landRightDTO;
+	OrdinateDTO ordinateDTO;
+	SpatialElementDTO spatialElementDTO;
+	SpatialElementUnitDTO spatialElementUnitDTO;
+	LandRightDTO[] landRightDTOs;
+
+
 	/**
 	 * ********************************************************
-	 * тэги файла xml false - закрытый тэг , true - открытый
+	 * Константы названия атрибутов
 	 * ********************************************************
 	 */
 
@@ -89,9 +101,30 @@ public class Region_CadastrHandler extends DefaultHandler {
 	final static String NAME = "Name";
 	final static String STATE = "State";
 	final static String DATE_CR = "DateCreated";
-	//-------INIT-----------------------
+	final static String CATEGORY = "Category";
+	final static String UTILIZ = "ByDoc";
+	final static String CADASTRAL_COST_VALUE = "Value";
+	final static String CADASTRAL_COST_UNIT = "Unit";
+	//-------UNIT-----------------------
+	final static String ENT_SYS = "Ent_Sys";
 	final static String TOTAL = "Total";
 	final static String UNIT = "Unit";
+	final static String TYPE_UNIT = "Type_Unit";
+	final static String SU_NMB = "Su_Nmb";
+	final static String X = "X";
+	final static String Y = "Y";
+	final static String ORD = "Ord_Nmb";
+	//-------Bounds----------------------
+	final static String ENT_SYS_BOUNDS = "Ent_Sys";
+	//-------Coord-Sys----------------------
+	final static String NAME_COORD_SYS = "Name";
+	final static String CS_ID = "Cs_Id";
+
+	/**
+	 * ********************************************************
+	 * тэги файла xml false - закрытый тэг , true - открытый
+	 * ********************************************************
+	 */
 
 	boolean t_AreaM = false;//-----Area all
 	//--------Parcels-----------
@@ -105,12 +138,14 @@ public class Region_CadastrHandler extends DefaultHandler {
 	boolean t_inBounds = false;
 	boolean t_Placed = false;
 	//---------------------------
+	boolean t_Address = false;
 	boolean t_Code_OKATO = false;//-----Address (In Location)
 	boolean t_Code_KLADR = false;
 	boolean t_Region = false;
 	boolean t_District = false;
 	boolean t_Locality = false;
 	boolean t_Street = false;
+	boolean t_Level1 = false;
 	boolean t_Note = false;
 	//---------------------------
 	boolean t_Category = false;
@@ -118,8 +153,8 @@ public class Region_CadastrHandler extends DefaultHandler {
 	//---------------------------
 	boolean t_Rights = false;//-----Rights
 	boolean t_Right = false;
-	boolean t_Name = false;
-	boolean t_Type = false;
+	boolean t_Name_R = false;
+	boolean t_Type_R = false;
 	//---------------------------
 	boolean t_CadastralCost = false;
 	//-------OMS_Points-----------
@@ -130,28 +165,200 @@ public class Region_CadastrHandler extends DefaultHandler {
 	boolean t_PKlass = false;
 	boolean t_OrdX = false;
 	boolean t_OrdY = false;
+	//-------SpatialData----------
+	boolean t_SpatialData = false;
+	boolean t_Entity_Spatial = false;
+	boolean t_Spatial_Element = false;
+	boolean t_Spelement_Unit = false;
+	boolean t_Ordinate = false;
+	//-------Bounds---------------
+	boolean t_Bounds = false;
+	boolean t_Bound = false;
+	boolean t_AccountNumber = false;
+	boolean t_Boundaries = false;
+	boolean t_Boundary = false;
+	boolean t_Documents = false;
+	boolean t_Document = false;
+	boolean t_Name_B = false;
+	boolean t_Number = false;
+	boolean t_IssueOrgan = false;
+	boolean t_MunicipalBoundary = false;
+	boolean t_Name_MB = false;
+	//-------Coord_System---------
+	boolean t_Coord_System = false;
+	//-------Certification_Doc-----
+	boolean t_Certification_Doc = false;
+	boolean t_Organization = false;
+	boolean t_Date = false;
+	boolean t_Number_Cert = false;
+	boolean t_Appointment = false;
+	boolean t_FIO = false;
 
-
+	java.text.SimpleDateFormat dateFormat = new java.text.SimpleDateFormat(LandsImporter.YYYY_MM_DD);
 
 
 	@Override
 	public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
-		super.startElement(uri, localName, qName, attributes);
 
+
+		if (qName.equalsIgnoreCase("Area")) { //-------------Area
+			t_Area = true;
+		}
+		if (qName.equalsIgnoreCase("Area") && t_Area) {
+			t_AreaIn = true;
+		}
+		if (qName.equalsIgnoreCase("Unit") && t_Area) {
+			t_Unit = true;
+		}
+		if (qName.equalsIgnoreCase("Location")) { //--------Location
+			t_Location = true;
+		}
+		if (qName.equalsIgnoreCase("inBounds")) {
+			t_inBounds = true;
+		}
+		if (qName.equalsIgnoreCase("Placed")) {
+			t_Placed = true;
+		}
+
+		if (qName.equalsIgnoreCase("Address")) { //-------Address
+			t_Address = true;
+			addressDTO = new AddressDTO();
+		}
+		if (qName.equalsIgnoreCase("Code_OKATO")) {
+			t_Code_OKATO = true;
+		}
+		if (qName.equalsIgnoreCase("Code_KLADR")) {
+			t_Code_KLADR = true;
+		}
+
+		if (qName.equalsIgnoreCase("Region")) {
+			t_Region = true;
+		}
+		if (qName.equalsIgnoreCase("District")) {
+			t_District = true;
+			addressDTO.setDistrictName(attributes.getValue("Name"));
+			addressDTO.setDistrictType(attributes.getValue("Type"));
+		}
+		if (qName.equalsIgnoreCase("Locality")) {
+			t_Locality = true;
+			addressDTO.setLocalityName(attributes.getValue("Name"));
+			addressDTO.setLocalityType(attributes.getValue("Type"));
+		}
+		if (qName.equalsIgnoreCase("Street")) {
+			t_Street = true;
+			addressDTO.setStreetName(attributes.getValue("Name"));
+			addressDTO.setStreetType(attributes.getValue("Type"));
+		}
+
+		if (qName.equalsIgnoreCase("Level1")) {
+			t_Level1 = true;
+			addressDTO.setLevelType(attributes.getValue("Type"));
+			addressDTO.setLevelValue(attributes.getValue("Value"));
+		}
+		if (qName.equalsIgnoreCase("Note")) {
+			t_Note = true;
+		}
+		if (qName.equalsIgnoreCase("Category")) {
+			t_Category = true;
+			landDTO.setCategory(attributes.getValue(CATEGORY));
+		}
+
+		if (qName.equalsIgnoreCase("Utilization")) {
+			t_Utilization = true;
+			landDTO.setUtilizationByDoc(attributes.getValue(UTILIZ));
+		}
+		if (qName.equalsIgnoreCase("Rights")) { //---------Rights
+			t_Rights = true;
+		}
+		if (qName.equalsIgnoreCase("Right")) {
+			t_Right = true;
+			landRightDTO = new LandRightDTO();
+		}
+		if (qName.equalsIgnoreCase("Name")) {
+			t_Name_R = true;
+		}
+		if (qName.equalsIgnoreCase("Type")) {
+			t_Type_R = true;
+		}
+		if (qName.equalsIgnoreCase("CadastralCost")) { //-----------Cadastral
+			t_CadastralCost = true;
+			landDTO.setCadastralCostValue(Double.valueOf(attributes.getValue(CADASTRAL_COST_VALUE)));
+			landDTO.setCadastralCostUnit(Integer.valueOf(attributes.getValue(CADASTRAL_COST_UNIT)));
+		}
+		if (qName.equalsIgnoreCase("SpatialData")) { //-----SpatialData
+			t_SpatialData = true;
+			entitySpatialDTO = new EntitySpatialDTO();
+		}
+		if (qName.equalsIgnoreCase("Entity_Spatial")) {
+			t_Entity_Spatial = true;
+			entitySpatialDTO.setEntSys(attributes.getValue(ENT_SYS));
+
+		}
+		if (qName.equalsIgnoreCase("Spatial_Element")) {
+			t_Spatial_Element = true;
+			spatialElementDTO = new SpatialElementDTO();
+		}
+		if (qName.equalsIgnoreCase("Spelement_Unit")) {
+			//TODO: Не понял что куда закинуть в массивы ordinates из атрибутов
+			t_Spelement_Unit = true;
+			spatialElementUnitDTO = new SpatialElementUnitDTO();
+			attributes.getValue(TYPE_UNIT);
+			attributes.getValue(SU_NMB);
+		}
+		if (qName.equalsIgnoreCase("Ordinate")) {
+			t_Ordinate = true;
+			ordinateDTO = new OrdinateDTO();
+			ordinateDTO.setX(Double.valueOf(attributes.getValue(X)));
+			ordinateDTO.setY(Double.valueOf(attributes.getValue(Y)));
+			ordinateDTO.setOrdNumber(Integer.valueOf(attributes.getValue(ORD)));
+
+		}
 	}
 
 	@Override
 	public void endElement(String uri, String localName, String qName) throws SAXException {
-		super.endElement(uri, localName, qName);
+		if (t_Address && qName.endsWith("Address")) {
+			landDTO.setAddress(addressDTO);
+			t_Address = false;
+		}
+
+		if (t_Right && qName.endsWith("Right")) {
+			t_Right = false;
+			//landRightDTOs
+		}
+
+		if (t_Rights && qName.endsWith("Rights")) {
+			landDTO.setRights(landRightDTOs);
+			t_Rights = false;
+		}
 	}
 
 	@Override
 	public void characters(char[] ch, int start, int length) throws SAXException {
-		super.characters(ch, start, length);
+		if (t_Area && t_AreaIn) {
+			landDTO.setArea(Double.parseDouble(String.valueOf(ch, start, length)));
+			t_AreaIn = false;
+		}
+		if (t_Area && t_Unit) {
+			landDTO.setAreaUnit(String.valueOf(ch, start, length));
+			t_Unit = false;
+		}
+	}
+
+	@Override
+	public void error(SAXParseException e) {
+		e.printStackTrace();
 	}
 
 	@Override
 	public void fatalError(SAXParseException e) throws SAXException {
 		super.fatalError(e);
+		e.printStackTrace();
 	}
+
+	@Override
+	public void warning(SAXParseException e) {
+		e.printStackTrace();
+	}
+
 }
