07e1e1539bc54f461c9751558be0fda6f08ad967
donchenko-y <y.donchenko@sovzond.ru>
2015-12-08 13:09:08 +0300

    MGIS-52 -В поле По справочнику (вкладка Общие сведения, блок Разрешенное использование) внесены изменения отображения с родителями, перевода на следущую строку

--- mgis-initial-data/allowed-usage/LandAllowedUsage.sql
+++ mgis-initial-data/allowed-usage/LandAllowedUsage.sql
@@ -248,3 +248,16 @@ INSERT INTO nc_land_allowed_usage (id,number,classification_code,name) VALUES (n
 INSERT INTO nc_land_allowed_usage (id,number,classification_code,name) VALUES (nextval('nc_land_allowed_usage_seq'),'146.3','146003000000','Для размещения водохозяйственных сооружений');
 INSERT INTO nc_land_allowed_usage (id,number,classification_code,name) VALUES (nextval('nc_land_allowed_usage_seq'),'146.4','146004000000','Для иного разрешенного использования, не связанного с водопользованием');
 INSERT INTO nc_land_allowed_usage (id,number,classification_code,name) VALUES (nextval('nc_land_allowed_usage_seq'),'147','147000000000','Земли запаса (неиспользуемые)');
+--Update Parent id
+UPDATE nc_land_allowed_usage u4
+SET parent_id = q1.parent_id
+FROM (SELECT id AS parent_id, child_id AS child_id
+      FROM nc_land_allowed_usage u2
+        LEFT JOIN (SELECT
+                     CASE WHEN position('.' IN number) > 0
+                       THEN regexp_replace(number, '\.\d+$', '')
+                     ELSE '' END AS parent_number,
+                     id          AS child_id,
+                     number      AS child_number
+                   FROM nc_land_allowed_usage u1) u3 ON u2.number = u3.parent_number) q1
+WHERE q1.child_id = u4.id;
