9b61e93da1d0434d248a7de513a215f122243a27
Ալեքսանդր Առաքելյան <bambrikii@gmail.com>
2015-12-13 13:23:09 +0300

    - reports progress status, - reports optional generate handler.

--- mgis-web/src/main/webapp/app2/reports/report-module.js
+++ mgis-web/src/main/webapp/app2/reports/report-module.js
@@ -3,6 +3,8 @@ angular.module("mgis.reports.report", [
 	"mgis.commons.forms",
 	"mgis.reports.report.service"
 ])
+	.constant("REPORT_GENERATION_IN_PROGRESS", "InProgress")
+	.constant("REPORT_GENERATION_COMPLETE", "Complete")
 	.factory("ReportsReportCRUDService", function ($rootScope, $filter, ReportsReportService, MGISCommonsModalForm, NUMBER_PATTERN) {
 		var edit = function (item, refresh) {
 			var modalScope = $rootScope.$new();
@@ -102,26 +104,50 @@ angular.module("mgis.reports.report", [
 			restrict: "E",
 			scope: {
 				filter: "@",
-				data: "="
+				data: "=",
+				generateHandler: "&"
 			},
 			templateUrl: "app2/reports/report-generator.htm",
-			controller: function ($scope, ReportsReportService, BytesConversionService) {
+			link: function (scope, elem, attrs) {
+
+				scope.hasGenerateHandler = function () {
+					return angular.isDefined(attrs.generateHandler);
+				}
+			},
+			controller: function ($scope, ReportsReportService, BytesConversionService, REPORT_GENERATION_IN_PROGRESS, REPORT_GENERATION_COMPLETE) {
+				$scope.reportStatus = {};
 				ReportsReportService.get(null, 0, 0, $scope.filter).then(function (data) {
 					$scope.availableReports = data.list;
 				});
+
+				function doGenerateReport(bytes, filename) {
+					var array = BytesConversionService.base64ToBlob(bytes);
+					var blob = new Blob([array], {type: "application/octet-stream"});
+					var a = window.document.createElement('a');
+					var objectUrl = window.URL.createObjectURL(blob);
+					a.href = objectUrl;
+					a.download = filename;
+					document.body.appendChild(a)
+					a.click();
+					document.body.removeChild(a)
+				}
+
 				$scope.generateReport = function (reportId, format) {
 					var json = JSON.stringify($scope.data);
-					ReportsReportService.generate(json, reportId, format).then(function (data) {
-						var array = BytesConversionService.base64ToBlob(data.bytes);
-						var blob = new Blob([array], {type: "application/octet-stream"});
-						var a = window.document.createElement('a');
-						var objectUrl = window.URL.createObjectURL(blob);
-						a.href = objectUrl;
-						a.download = data.filename;
-						document.body.appendChild(a)
-						a.click();
-						document.body.removeChild(a)
-					});
+					if ($scope.hasGenerateHandler()) {
+						$scope.reportStatus[reportId] = {status: REPORT_GENERATION_IN_PROGRESS};
+						var startDate = new Date();
+						var data = $scope.generateHandler({reportId: reportId, format: format});
+						doGenerateReport(data.bytes, data.filename);
+						$scope.reportStatus[reportId] = {status: REPORT_GENERATION_COMPLETE, time: new Date() - startDate};
+					} else {
+						$scope.reportStatus[reportId] = {status: REPORT_GENERATION_IN_PROGRESS};
+						var startDate = new Date();
+						ReportsReportService.generate(json, reportId, format).then(function (data) {
+							doGenerateReport(data.bytes, data.filename);
+							$scope.reportStatus[reportId] = {status: REPORT_GENERATION_COMPLETE, time: new Date() - startDate};
+						});
+					}
 				}
 			}
 		}
