a823f71cfdabbdf4ebd29854806c91274f5125d2
Alexander Arakelyan <arakelyan@sovzond.ru>
2015-12-17 15:30:48 +0300

    MGIS-75 - Доработать таблицу в блоке Входящие земельные участки или части земельного участка, MGIS-76 - Доработать таблицу в блоке Входящие объекты (расположенные на земельном участке).

--- mgis-web/src/main/webapp/app2/lands/land-selector/land-selector-module.js
+++ mgis-web/src/main/webapp/app2/lands/land-selector/land-selector-module.js
@@ -10,52 +10,67 @@ angular.module("mgis.lands.land-selector", ["ui.bootstrap",
 				lands: "="
 			},
 			templateUrl: "app2/lands/land-selector/lands-selector-component.htm",
-			controller: function ($scope, $rootScope, MGISCommonsModalForm, LandsLandService, LandsLandCRUDService, CommonsPagerManager) {
+			controller: function ($scope, $rootScope, MGISCommonsModalForm) {
 				$scope.select = function () {
 					var modalScope = $rootScope.$new();
-					modalScope.currentPage = 1;
-					modalScope.itemsPerPage = CommonsPagerManager.pageSize();
-					function updateList() {
-						LandsLandService.get("", CommonsPagerManager.offset(modalScope.currentPage), modalScope.itemsPerPage,
-							modalScope.cadastralNumber,
-							ids
-						).then(function (data) {
-								$scope.landsSelectorPager = data;
-								$scope.selectedIds = {};
-								for (var i in data.list) {
-									var land = data.list[i];
-									if (ids.indexOf(land.id) > -1) {
-										$scope.selectedIds[land.id] = {checked: true, cadastralNumber: land.cadastralNumber};
-									}
-								}
-							}
-						);
+					modalScope.selectedLands = new Array();
+					for (var i in $scope.lands) {
+						var land = $scope.lands[i];
+						modalScope.selectedLands.push(land);
 					}
 
-					modalScope.add = function () {
-						LandsLandCRUDService.add(updateList);
-					}
-					modalScope.edit = function (id) {
-						LandsLandCRUDService.edit(id, updateList);
-					}
-					modalScope.remove = function (id) {
-						LandsLandCRUDService.remove(id, updateList);
-					}
-					modalScope.pageChanged = function () {
-						updateList();
-					}
-					modalScope.find = function (cadastralNumber) {
-						updateList();
-					}
 
 					MGISCommonsModalForm.edit("app2/lands/land-selector/land-selector-search.htm", modalScope, function (scope, modalInstance) {
+						$scope.lands = scope.selectedLands;
 						modalInstance.close();
-					});
+					}, {windowClass: "mgis-land-modal-form"});
+
 				}
 			}
 		}
 	})
-	.controller("LandsLandSelectorController", function () {
+	.controller("LandsLandSelectorController", function ($scope, LandsLandService, LandsLandCRUDService, CommonsPagerManager) {
+		$scope.currentPage = 1;
+		$scope.itemsPerPage = CommonsPagerManager.pageSize();
+		function updateList() {
+			console.log($scope.currentPage + " " + $scope.itemsPerPage);
+			LandsLandService.get(null, CommonsPagerManager.offset($scope.currentPage), $scope.itemsPerPage, $scope.cadastralNumber)
+				.then(function (data) {
+					$scope.landsSelectorPager = data;
+				});
+		}
 
+		$scope.addItem = function () {
+			LandsLandCRUDService.add(updateList);
+		}
+		$scope.editItem = function (id) {
+			LandsLandCRUDService.edit(id, updateList);
+		}
+		$scope.removeItem = function (id) {
+			LandsLandCRUDService.remove(id, updateList);
+		}
+		$scope.pageChanged = function () {
+			updateList();
+		}
+		$scope.find = function (cadastralNumber) {
+			updateList();
+		}
+		$scope.selectItem = function (land) {
+			for (var i in $scope.selectedLands) {
+				if ($scope.selectedLands[i].id == land.id) {
+					return;
+				}
+			}
+			$scope.selectedLands.push(land);
+		}
+		$scope.deselect = function (landId) {
+			for (var i in $scope.selectedLands) {
+				if ($scope.selectedLands[i].id == landId) {
+					$scope.selectedLands.splice(i, 1);
+				}
+			}
+		}
+
+		updateList();
 	})
 ;
