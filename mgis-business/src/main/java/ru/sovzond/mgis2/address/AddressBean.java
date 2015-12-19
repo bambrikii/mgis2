9202eb8c34b28a9c679d3455dfb58f8b5bae234e
Ալեքսանդր Առաքելյան <bambrikii@gmail.com>
2015-12-19 19:54:01 +0300

    - jai_* dependencies removed as not used for now, - address import exception messages detailed.

--- mgis-business/src/main/java/ru/sovzond/mgis2/address/AddressBean.java
+++ mgis-business/src/main/java/ru/sovzond/mgis2/address/AddressBean.java
@@ -50,28 +50,80 @@ public class AddressBean extends CRUDBeanBase<Address> {
 	}
 
 	public List<Address> find(AddressFilter filter) {
-		KLADRLocality subject = null;
-		if (filter.subject()) {
-			subject = kladrLocalityDao.findSubject(filter.getSubject(), filter.getSubjectType()).get(0);
-		} else if (filter.subjectCode()) {
-			subject = kladrLocalityDao.findSubjectByCode(filter.getSubjectCode());
-		}
-		KLADRLocality region = kladrLocalityDao.findRegion(subject != null ? subject.getCode() : null, filter.getRegion(), filter.getRegionType()).get(0);
-		KLADRLocality locality = kladrLocalityDao.findLocality(region.getCode(), filter.getLocality(), filter.getLocalityType()).get(0);
-		List<KLADRStreet> streets = kladrStreetDao.findStreet(locality.getCode(), filter.getStreet(), filter.getStreetType());
-		KLADRStreet street;
-		switch (streets.size()) {
-			case 1:
-				street = streets.get(0);
-				break;
-			default:
-				throw new IllegalArgumentException(
-						"No street found: " + filter.getStreet() + ", " + filter.getStreetType()
-								+ ". Subject: " + subject.getCode() + ", " + subject.getName() + ", " + subject.getSocr()
-								+ ". Region: " + region.getCode() + ", " + region.getName() + ", " + region.getSocr()
-								+ ". Locality: " + locality.getCode() + ", " + locality.getName() + ", " + locality.getSocr()
-				);
-		}
+		KLADRLocality subject = findSubject(filter);
+		KLADRLocality region = findRegion(filter, subject);
+		KLADRLocality locality = findLocality(filter, subject, region);
+		KLADRStreet street = findStreet(filter, subject, region, locality);
 		return dao.find(subject, region, locality, street, filter.getHome(), filter.getHousing(), filter.getBuilding(), filter.getApartment());
+	}
+
+	private KLADRStreet findStreet(AddressFilter filter, KLADRLocality subject, KLADRLocality region, KLADRLocality locality) {
+		List<KLADRStreet> streets = kladrStreetDao.findStreet(locality.getCode(), filter.getStreet(), filter.getStreetType());
+		int size = streets.size();
+		switch (size) {
+			case 1:
+				return streets.get(0);
+			default:
+				throw streetNotFoundException(size, filter, subject, region, locality);
+		}
+	}
+
+	private KLADRLocality findLocality(AddressFilter filter, KLADRLocality subject, KLADRLocality region) {
+		List<KLADRLocality> localities = kladrLocalityDao.findLocality(region.getCode(), filter.getLocality(), filter.getLocalityType());
+		int size = localities.size();
+		switch (size) {
+			case 1:
+				return localities.get(0);
+			default:
+				throw localityNotFoundException(size, filter, subject, region);
+		}
+	}
+
+	private KLADRLocality findRegion(AddressFilter filter, KLADRLocality subject) {
+		List<KLADRLocality> regions = kladrLocalityDao.findRegion(subject != null ? subject.getCode() : null, filter.getRegion(), filter.getRegionType());
+		int size = regions.size();
+		switch (size) {
+			case 1:
+				return regions.get(0);
+			default:
+				throw regionNotFoundException(size, filter, subject);
+		}
+	}
+
+	private KLADRLocality findSubject(AddressFilter filter) {
+		if (filter.subject()) {
+			List<KLADRLocality> subjects = kladrLocalityDao.findSubject(filter.getSubject(), filter.getSubjectType());
+			int size = subjects.size();
+			switch (size) {
+				case 1:
+					return subjects.get(0);
+				default:
+					throw subjectNotFoundException(size, filter);
+			}
+		} else if (filter.subjectCode()) {
+			KLADRLocality subject = kladrLocalityDao.findSubjectByCode(filter.getSubjectCode());
+			if (subject == null) {
+				throw subjectNotFoundException(0, filter);
+			}
+			return subject;
+		} else {
+			throw new IllegalArgumentException("No Subject filter defined.");
+		}
+	}
+
+	private IllegalArgumentException subjectNotFoundException(int size, AddressFilter filter) {
+		return new IllegalArgumentException(size + " Subjects found: " + filter.getSubject() + ", " + filter.getSubjectType() + ", " + filter.getSubjectCode() + " .");
+	}
+
+	private IllegalArgumentException regionNotFoundException(int size, AddressFilter filter, KLADRLocality subject) {
+		return new IllegalArgumentException(size + " Regions found: " + filter.getRegion() + ", " + filter.getRegionType() + ". Subject: " + subject.getName() + ", " + subject.getSocr() + " .");
+	}
+
+	private IllegalArgumentException localityNotFoundException(int size, AddressFilter filter, KLADRLocality subject, KLADRLocality region) {
+		return new IllegalArgumentException(size + " Localities found: " + filter.getLocality() + ", " + filter.getLocalityType() + ". Subject: " + subject.getName() + ", " + subject.getSocr() + ". Region: " + region.getName() + ", " + region.getSocr() + " .");
+	}
+
+	private IllegalArgumentException streetNotFoundException(int size, AddressFilter filter, KLADRLocality subject, KLADRLocality region, KLADRLocality locality) {
+		return new IllegalArgumentException(size + " Streets found: " + filter.getStreet() + ", " + filter.getStreetType() + ". Subject: " + subject.getCode() + ", " + subject.getName() + ", " + subject.getSocr() + ". Region: " + region.getCode() + ", " + region.getName() + ", " + region.getSocr() + ". Locality: " + locality.getCode() + ", " + locality.getName() + ", " + locality.getSocr());
 	}
 }
